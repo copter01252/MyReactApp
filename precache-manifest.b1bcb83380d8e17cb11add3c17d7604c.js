13ff44b3c7d7b30eccef644690da82f7536d3acc
Krit <youknow7451@gmail.com>
2019-05-12 01:09:43 +0700

    Updates

---
+++ precache-manifest.b1bcb83380d8e17cb11add3c17d7604c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce397724a7a03dd3aaaa3b27aafadcaa",
+    "url": "/MyReactApp/index.html"
+  },
+  {
+    "revision": "314a91f397b02d7a90d7",
+    "url": "/MyReactApp/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4532ad8c09e2facb0441",
+    "url": "/MyReactApp/static/css/main.48f543a2.chunk.css"
+  },
+  {
+    "revision": "314a91f397b02d7a90d7",
+    "url": "/MyReactApp/static/js/2.4eef37c8.chunk.js"
+  },
+  {
+    "revision": "4532ad8c09e2facb0441",
+    "url": "/MyReactApp/static/js/main.1844dffc.chunk.js"
+  },
+  {
+    "revision": "20d790f1628c74a78b0b",
+    "url": "/MyReactApp/static/js/runtime~main.e0e68d30.js"
+  }
+]);
